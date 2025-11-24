7afe0bc6e5b228c0361e3bfd70c100071995406f
vsyang <yang.vanessa85@gmail.com>
2025-11-24 16:49:14 -0700

    re-ran npm run swagger so api-docs shows gifts and fixed routes index.js so api-docs shows ahead of gifts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 
 /* ******************************************
+ * Swagger API-Docs -vy
+ *******************************************/
+router.use('/', require('./swagger'));
+
+/* ******************************************
+ * Main Resource Routes -vy
+ *******************************************/
+router.use('/gifts', require('./gifts'));
+
+
+
+/* ******************************************
  * Basic Home Route -vy
  *******************************************/
 router.get('/', (req, res) => {
@@ -8,19 +20,10 @@ router.get('/', (req, res) => {
     <h1>Welcome to the Holiday Party Planner</h1>
     <p>Click a link below to view the different routes:</p>
     <ul>
+      <li><a href="/gifts">Gifts Collection</a></li>
       <li><a href="/api-docs">Swagger Documentation</a></li>
     </ul>
   `);
 });
-
-/* ******************************************
- * Main Resource Routes -vy
- *******************************************/
-router.use('/gifts', require('./gifts'));
-
-/* ******************************************
- * Swagger Docs -vy
- *******************************************/
-router.use('/', require('./swagger'));
 
 module.exports = router;
